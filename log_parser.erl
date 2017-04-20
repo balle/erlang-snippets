5b917dba323143d2e0a3a96310b1597808a370bf
Bastian Ballmann <basti@espy.codekid.net>
2017-04-20 08:21:14 +0200

    playing around with elixir

--- log_parser.erl
+++ log_parser.erl
@@ -6,7 +6,7 @@
 parse_syslog(Line) ->
     %% Feb 26 13:30:14 openbsd apmd: cannot set hw.perfpolicy
     %% Feb 26 13:32:56 openbsd pulseaudio[14084]: [(null)] main.c: Daemon startup failed.
-    {ok, MatchSyslog} = re:compile("^(?<Month>\w{3}) (?<Day>\d{2}) (?<Hour>\d{2}):(?<Minute>\d{2}):(?<Second>\d{2}) (?<Host>\w+?) (<Command>\w+?)(\[(?<Pid>\d+?)\])?: (<?Message>.+)$"),
+    {ok, MatchSyslog} = re:compile("^(?<Month>\w{3})\s(?<Day>\d{2})\s(?<Hour>\d{2}):(?<Minute>\d{2}):(?<Second>\d{2})\s(?<Host>\w+?)\s(?<Command>\w+?)(\[(?<Pid>\d+?)\])?:\s(?<Message>.+)$"),
     {match, ParsedLine} = re:run(Line, MatchSyslog),
     persist_data(ParsedLine).
 
