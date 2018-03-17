3dedb3b5a77f69fade27b429aa4338c8b2871541
Bastian Ballmann <balle@codekid.net>
2018-03-17 23:23:33 +0100

    regexp now working

--- log_parser.erl
+++ log_parser.erl
@@ -1,17 +1,38 @@
 -module(log_parser).
--export([parse_syslog/1]).
--include_lib("stdlib/include/qlc.hrl").
--include("log.hrl").
+-export([read_file/1, parse_syslog/1]).
+
+read_file(File) ->
+    {ok, FileHandle} = file:open(File, [read]), 
+    Data = get_lines(FileHandle),
+    file:close(FileHandle),
+    Data.
+
+get_lines(FileHandle) ->
+    Buffer = [],
+    get_lines(FileHandle, Buffer).
+
+get_lines(FileHandle, Buffer) ->
+    Line = io:get_line(FileHandle, ""),
+
+    if Line /= eof ->
+        get_lines(FileHandle, Buffer ++ [string:strip(Line, right, $\n)]);
+    true ->
+        Buffer
+    end.
 
 parse_syslog(Line) ->
-    %% Feb 26 13:30:14 openbsd apmd: cannot set hw.perfpolicy
-    %% Feb 26 13:32:56 openbsd pulseaudio[14084]: [(null)] main.c: Daemon startup failed.
-    {ok, MatchSyslog} = re:compile("^(?<Month>\w{3})\s(?<Day>\d{2})\s(?<Hour>\d{2}):(?<Minute>\d{2}):(?<Second>\d{2})\s(?<Host>\w+?)\s(?<Command>\w+?)(\[(?<Pid>\d+?)\])?:\s(?<Message>.+)$"),
-    {match, ParsedLine} = re:run(Line, MatchSyslog),
-    persist_data(ParsedLine).
+    %% Mar 17 16:23:19 writeordie /bsd: /tmp force dirty (dangling 164 inflight 0)
+    %% Mar 17 16:25:57 writeordie /bsd: urtwn0 detached
+    {ok, MatchSyslog} = re:compile("^(?<Month>\\w{3})\\s(?<Day>\\d{2})\\s(?<Hour>\\d{2}):(?<Minute>\\d{2}):(?<Second>\\d{2})\\s(?<Host>\\w+?)\\s(?<Command>.+?)(\[(?<Pid>\\d+?)\])?:\\s(?<Message>.+)$"),
+
+    case re:run(Line, MatchSyslog) of
+        {match, ParsedLine} -> persist_data(ParsedLine);
+	notmatch -> []
+    end.
 
 persist_data([Month, Day, Hour, Minute, Second, Host, Command, Pid, Message]) ->
     Row = #log{month=Month, day=Day, hour=Hour, minute=Minute, second=Second, host=Host, command=Command, pid=Pid, message=Message},
     mnesia:write(Row).
 
-    
+
+
